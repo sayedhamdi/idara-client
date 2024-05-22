2bbaa023f0818a3eeaea3c7c7639b6e2e9cd07d4
Sayed Hamdi <32652375+sayedhamdi@users.noreply.github.com>
2024-05-22 23:17:15 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer,toast } from 'react-toastify';
 
 import BlogsList from "./components/BlogsList";
 import CreateBlog from "./components/CreateBlog";
 import { useEffect, useState } from 'react';
+import { Route, Routes, useParams } from 'react-router-dom';
 
-function App(){
+function Blogs(){
   const [blogs, setBlogs] = useState([]);
   const [loading,setLoading] =useState(true)
   useEffect(()=>{
@@ -25,8 +26,111 @@ function App(){
       <ToastContainer />
       <CreateBlog setLoading={setLoading}/>
       <br/>
-      {loading ? "....." : <BlogsList blogs={blogs} />}
+      {loading ? "....." : <BlogsList blogs={blogs} setLoading={setLoading} />}
     </div>
   )
 }
-export default App
+
+function App(){
+  return (
+    <Routes>
+      <Route path="/" element={<Blogs />}/>
+      <Route path="/blogs/edit/:id" element={<EditBlog />}/>
+    </Routes>
+  )
+}
+
+function EditBlog({setLoading}) {
+    let [title,setTitle] = useState("");
+    let [author,setAuthor]= useState("");
+    let [image,setImage] = useState("");
+    let [content,setContent] =useState("")
+    let {id}  = useParams();
+    function editBlog(e){
+        e.preventDefault()
+        let newBlog = {title,author,image,content}
+        fetch("http://localhost:8000/blogs/"+id,{
+            method:"PUT",
+            headers: {
+                "content-type":"application/json",
+            },
+            body : JSON.stringify(newBlog)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            if(data.message){
+                toast.error(data.message)
+            }else{
+                toast.success("Created a new blog "+title);
+                setAuthor("")
+                setContent("")
+                setImage("")
+                setTitle("")
+                setLoading(true)
+            }
+        })
+        
+         
+    }
+
+    useEffect(()=>{
+      fetch("http://localhost:8000/blogs/"+id)
+      .then(res=>res.json())
+      .then(data=>{
+        setTitle(data.title);
+        setAuthor(data.author)
+        setImage(data.image)
+        setContent(data.content)
+      })
+    },[])
+  return (
+    <div>
+      <form>
+      
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={title}
+            onChange={(e)=>setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="author">Author:</label>
+          <input
+            type="text"
+            id="author"
+            name="author"
+            value={author}
+            onChange={(e)=>setAuthor(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="image">Image URL:</label>
+          <input
+            type="text"
+            id="image"
+            name="image"
+            value={image}
+            onChange={(e)=>setImage(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="content">Content:</label>
+          <textarea
+            id="content"
+            name="content"
+            value={content}
+            onChange={(e)=>setContent(e.target.value)}
+          />
+        </div>
+        <button type="submit" onClick={editBlog}>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+
+export default App;
--- src/components/BlogsList.jsx
+++ src/components/BlogsList.jsx
@@ -1,7 +1,21 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
-export default function BlogsList({blogs}) {
-  
+export default function BlogsList({blogs,setLoading}) {
+   async function deleteBlog(id){
+    try{
+      let res = await fetch("http://localhost:8000/blogs/"+id,{method:"DELETE"});
+      let data = await res.json()
+      toast.success("deleted element ! sahit")
+      console.log(data)
+      setLoading(true)
+    }catch(e){
+      toast.error("error while deleting")
+      console.log(e)
+    }
+    
+   }
   return (
     <div>
       <table>
@@ -23,17 +37,17 @@ export default function BlogsList({blogs}) {
             ? "No blogs"
             : blogs?.map((blog) => (
                 <tr key={blog.key}>
-                  <td>{blog.id}</td>
+                  <td>{blog._id}</td>
                   <td>{blog.title}</td>
                   <td>{blog.content}</td>
                   <td>{blog.author}</td>
                   <td><img width="200px" src={blog.image} /></td>
                   <td>{blog.createdAt}</td>
                   <td>
-                    <button>Update</button>
+                    <Link to={`blogs/edit/${blog._id}`}><button>Update</button></Link>
                   </td>
                   <td>
-                    <button>Delete</button>
+                    <button onClick = {()=>deleteBlog(blog._id)} style={{background:"red"}}>Delete</button>
                   </td>
                 </tr>
               ))}
